af238970a5fb94fdd73c854295255d7244efdd68
Rocky S Lee <rlee52986@gmail.com>
2021-05-19 22:04:48 -0500

    Added like component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
+import Like from './common/like';
 
 class Movies extends Component {
     state = { 
@@ -11,15 +12,31 @@ class Movies extends Component {
         this.setState({ movies });
      }
 
+     handleLike = (movie) => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = { ...movies[index] };
+        movies[index].liked = !movies[index].liked;
+        this.setState({ movies });
+     };
+
     render() { 
-        return <table className="table">
+        const { length: count } = this.state.movies;
+
+        if (this.state.movies.length === 0) return <p>There are no movies in the database.</p>;
+
+        return (
+            <React.Fragment>
+            <p>Showing {count} movies in the database.</p>
+        <table className="table">
             <thead>
                 <tr>
                     <th>Title</th>
                     <th>Genre</th>
                     <th>Stock</th>
                     <th>Rate</th>
-                    <th></th>
+                    <th>Like</th>
+                    <th />
                 </tr>
             </thead>
             <tbody>
@@ -28,12 +45,15 @@ class Movies extends Component {
                         <td>{movie.genre.name}</td>
                         <td>{movie.numberInStock}</td>
                         <td>{movie.dailyRentalRate}</td>
-                        <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
-                    </tr>
-                ))}
-                
+                        <td>
+                            <Like liked={movie.liked} onClick={() => this.handleLike(movie)}/>
+                        </td>
+                        <td><button onClick={() => this.handleDelete()} className="btn btn-danger btn-sm">Delete</button></td>
+                    </tr>))}
             </tbody>
         </table>
+        </React.Fragment>
+        );
     }
 }
  
